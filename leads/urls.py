543853113bf9f2b2c314c670efe2777fe5bf4216
Jonathan <jhamilton2394@gmail.com>
2023-06-15 23:27:34 -0700

    Periodic commit

--- leads/urls.py
+++ leads/urls.py
@@ -5,9 +5,9 @@ from leads.views import lead_detail, lead_list, lead_create, lead_update, lead_d
 app_name = "leads"
 
 urlpatterns = [
-    path('', lead_list),
-    path('<int:pk>/', lead_detail),
+    path('', lead_list, name="lead-list"),
+    path('<int:pk>/', lead_detail, name="lead-detail"),
     path('create', lead_create, name="lead-create"),
     path('<int:pk>/update', lead_update, name="lead-update"),
-    path('<int:pk>/delete', lead_delete),
+    path('<int:pk>/delete', lead_delete, name="lead-delete"),
 ]
